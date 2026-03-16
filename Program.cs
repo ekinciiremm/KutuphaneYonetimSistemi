86a7c112b800ff9819f59ea106605b61dcd0afd5
ekinciiremm <iremeknc24@gmail.com>
2026-03-16 18:31:12 +0300

    Program.cs ve DbManager sınıfları SQL standartlarına göre revize edildi.

--- Program.cs
+++ Program.cs
@@ -1,55 +1,47 @@
+using Microsoft.Data.SqlClient;
+using OOPDeneme.DbManager; 
 using System;
-namespace kutuphaneYonetimi
+
+namespace OOPDeneme
 {
-    public class Kitaplar
-    {
-        //gizlendi. verilere doğrudan erişim yok.
-        private string _kitapAdi;
-        private int _StokAdedi;
-
-
-        public Kitaplar(string kitapAdi, int stokAdedi)
-        {
-            KitapAdi = kitapAdi;
-            StokAdedi = stokAdedi;
-        }
-        public string KitapAdi
-        {
-            get { return _kitapAdi; }
-            set { _kitapAdi = value; }
-        }
-        public int StokAdedi
-        {
-            get { return _StokAdedi; }
-            set
-            {
-                if (value > 0)
-                {
-                    _StokAdedi = value;
-                }
-                else
-                {
-                    Console.WriteLine("Kitap adedi sıfırdan küçük olamaz");
-                }
-            }
-        }
-
-        public void Yazdir()
-        {
-            Console.WriteLine($"Kitap Adı:{KitapAdi}, Stok Adedi:{StokAdedi}");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Kitaplar k1 = new Kitaplar("Mai ve Siyah", 34);
-            k1.Yazdir();
+            KutuphaneInterface servis = new DbManager.DbManager();
 
-            Kitaplar k2 = new Kitaplar("Aşk-ı Memnu", 12);
-            k2.Yazdir();
+            try
+            {
+               
+                Console.WriteLine("Kütüphane Veritabanı Kayıtları");
+                var kitaplar = servis.TumKitaplariGetir();
 
+                foreach (var kitap in kitaplar)
+                {
+                    Console.WriteLine("- " + kitap);
+                }
+
+                Console.WriteLine("Aranan Kitap Bulundu");
+                string sonuc = servis.KitapBul("Aşk-ı Memnu");
+                Console.WriteLine(sonuc);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bir hata oluştu: " + ex.Message);
+            }
+
+            Console.WriteLine("\nÇıkmak için bir tuşa basın...");
+            Console.ReadLine();
+
+           
         }
+
+        
     }
 }
+
+
+
+
+
+
