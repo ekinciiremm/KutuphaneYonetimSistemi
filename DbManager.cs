41fb27464e1f863395b5af999b0dc9bce9c11399
ekinciiremm <iremeknc24@gmail.com>
2026-03-25 18:40:51 +0300

    Update DbManager to load connection string from appsettings
    Refactor DbManager to use IConfiguration for connection string.

--- DbManager.cs
+++ DbManager.cs
@@ -1,13 +1,26 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace OOPDeneme.DbManager
 {
     internal class DbManager : IDbServis
     {
-        private string baglantiCumlesi = "Server=.\\SQLEXPRESS; Database=kutuphaneYonetimi; " +
-            "User Id=stajyer; Password=12345; TrustServerCertificate=True;";
+        private readonly string baglantiCumlesi;
+        public DbManager()
+        {
+            var yapilandir = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration config = yapilandir.Build();
+
+    
+            baglantiCumlesi = config.GetConnectionString("DefaultConnection");
+        }
+
 
         public void ExecuteCommand(string query, List<SqlParameter> parameters)
         {
@@ -29,5 +42,6 @@ namespace OOPDeneme.DbManager
             SqlCommand command = new SqlCommand(query, connection);
             return command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
         }
+
     }
 }
